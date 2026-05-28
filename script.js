69e894304eca238c99823a61b146f64a79a13a79
Gustavo <gustavo.melof388@gmail.com>
2026-05-28 18:12:59 -0300

    (WIP)Adicionei conversão para complemento de 1

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 function calcular() {
-   const operacao = document.getElementById("add").value;
+  const operacao = document.getElementById("add").value;
 
-    if (operacao == "soma") {
-      resultado = Number(document.getElementById("num1").value) + Number(document.getElementById("num2").value) 
+  if (operacao == "soma") {
+    resultado = Number(document.getElementById("num1").value) + Number(document.getElementById("num2").value); 
+  }
+  else if (operacao == "subtracao") {
+    resultado = Number(document.getElementById("num1").value) - Number(document.getElementById("num2").value); 
+  }
+  else if (operacao == "multiplicacao") {
+    resultado = Number(document.getElementById("num1").value) * Number(document.getElementById("num2").value);
+  }
+  else if (operacao == "divisao") {
+    resultado = Number(document.getElementById("num1").value) / Number(document.getElementById("num2").value);   
+  }
+  const mostrarResultado = document.getElementById("mostrarResultado");
+  mostrarResultado.textContent = resultado;
+}
+
+//Ainda falta fazer a escolha de bits, por enquanto o programa espera 8 bits, e limitar o input para apenas 0 e 1.
+function operacaoBinarios() {
+  const binarios = document.getElementById("binarios").value;
+  const numBinario = document.getElementById("numBinario").value;
+  let resultado = ""
+  for(let i = 0; i <=7; i++) {
+    let j = numBinario[i];
+    if (numBinario[i] == "0") {
+      j = "1";
     }
-    else if (operacao == "subtracao") {
-      resultado = Number(document.getElementById("num1").value) - Number(document.getElementById("num2").value) 
+    else {
+      j = "0";
     }
-    else if (operacao == "multiplicacao") {
-     resultado = Number(document.getElementById("num1").value) * Number(document.getElementById("num2").value)
-    }
-    else if (operacao == "divisao") {
-     resultado = Number(document.getElementById("num1").value) / Number(document.getElementById("num2").value)   
-    }
-    const resultadoConta = document.getElementById("resultadoConta");
-    resultadoConta.textContent = resultado
+    resultado = resultado + j;
+  }
+  const mostrarBinario = document.getElementById("mostrarBinario");
+  mostrarBinario.textContent = resultado
+
 }
